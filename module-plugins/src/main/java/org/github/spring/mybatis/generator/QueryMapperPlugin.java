537d60967d7ecb7c828d5ce7587311e62df370d0
JYDxl <1252504314@qq.com>
2017-10-11 16:47:42 +0800

    update

--- module-plugins/src/main/java/org/github/spring/mybatis/generator/QueryMapperPlugin.java
+++ module-plugins/src/main/java/org/github/spring/mybatis/generator/QueryMapperPlugin.java
@@ -12,9 +12,9 @@ import org.mybatis.generator.api.dom.java.TopLevelClass;
  */
 public class QueryMapperPlugin extends NaivePluginAdapter {
   @Override
-  public boolean clientGenerated(Interface anInterface, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-    anInterface.addAnnotation("@QueryMapper");
-    anInterface.addImportedType(new FullyQualifiedJavaType("org.github.spring.annotation.QueryMapper"));
-    return super.clientGenerated(anInterface, topLevelClass, introspectedTable);
+  public boolean clientGenerated(Interface mapperInterface, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    mapperInterface.addImportedType(new FullyQualifiedJavaType("org.github.spring.annotation.QueryMapper"));
+    mapperInterface.addAnnotation("@QueryMapper");
+    return super.clientGenerated(mapperInterface, topLevelClass, introspectedTable);
   }
 }
