83b722691f2a2b875fb65dafc17169e769ed37a5
JYDxl <1252504314l@gmail.com>
2017-10-14 18:07:14 +0800

    update

--- module-beans/src/main/java/org/github/spring/util/NaiveDateFormatUtil.java
+++
@@ -1,8 +0,0 @@
-package org.github.spring.util;
-
-import org.apache.commons.lang3.time.DateFormatUtils;
-
-/**
- * Created by JYD_XL on 2017/5/7.
- */
-public abstract class NaiveDateFormatUtil extends DateFormatUtils {}
--- module-beans/src/main/java/org/github/spring/util/NaiveDateUtil.java
+++
@@ -1,8 +0,0 @@
-package org.github.spring.util;
-
-import org.apache.commons.lang3.time.DateUtils;
-
-/**
- * Created by JYD_XL on 2017/5/7.
- */
-public abstract class NaiveDateUtil extends DateUtils {}
